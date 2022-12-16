2acab0910f170d04d6f21b45c1f2fb4fa6013cf3
Yuumi <kappa.and.bug@gmail.com>
2022-12-16 17:10:13 +0800

    create 10202121ptt_2.py

---
+++ 10202121ptt_2.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.ptt.cc/bbs/MobileComm/index.html"
+
+for i in range(3):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text,"html.parser")
+    sel = soup.find_all("a")
+    u = soup.select("div.btn-group.btn-group-paging a")
+    print("本頁的URL為" +url)
+    url = "https://www.ptt.cc"+ u[1]["href"]
+
+for s in sel:
+    print(s["href"],s.text)
